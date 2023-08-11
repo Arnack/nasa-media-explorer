de4389cccff3283818c4f05fc9ca93d41e19d2c6
arnack <gri-go-riy@mail.ru>
2023-08-11 13:17:48 +0100

    theme updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MainLayout from './components/layout/MainLayout';
 import AppRoutes from './AppRoutes';
 import { ChakraProvider } from '@chakra-ui/react';
 
+const chakraTheme = {
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+};
+
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={chakraTheme}>
       <MainLayout>
         <AppRoutes />
       </MainLayout>
